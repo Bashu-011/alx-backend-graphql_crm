f9a3c4bdaaf761c55e60c2b776d1cf03c95d7aad
Bashu-011 <bashu2609@gmail.com>
2025-10-30 15:26:20 +0300

    Corrections | Task 0 - Cronjobs

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -18,5 +18,14 @@ deleted, _ = Customer.objects.filter(last_order__lt=cutoff).delete()
 print(deleted)
 ")
 
+count=$(python manage.py shell -c "
+from crm.models import Customer
+from django.utils import timezone
+from datetime import timedelta
+cutoff = timezone.now() - timedelta(days=365)
+deleted, _ = Customer.objects.filter(last_order__lt=cutoff).delete()
+print(deleted)
+")
+
 #log the result
-echo \"[$TIMESTAMP] Deleted $DELETED inactive customers.\" >> $LOG_FILE
+echo \"[$TIMESTAMP] Deleted $count inactive customers.\" >> $LOG_FILE
