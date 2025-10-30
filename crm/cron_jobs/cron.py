950ec4042e0212cf9d27d74d567c9f2fce68dde3
Bashu-011 <bashu2609@gmail.com>
2025-10-30 15:55:48 +0300

    Task 2 | Cron job | Logging application health

---
+++ crm/cron_jobs/cron.py
@@ -0,0 +1,28 @@
+import datetime
+import requests
+
+def log_crm_heartbeat():
+    """
+    checks the GraphQL endpoint for a response
+    """
+    timestamp = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    message = f"{timestamp} CRM is live\n"
+
+    log_path = "/tmp/crm_heartbeat_log.txt"
+
+    try:
+        #graphQL endpoint check
+        response = requests.post(
+            "http://localhost:8000/graphql",
+            json={"query": "{ hello }"},
+            timeout=5
+        )
+        if response.status_code == 200:
+            message = f"{timestamp} CRM is alive (GraphQL OK)\n"
+        else:
+            message = f"{timestamp} CRM is alive (GraphQL FAILED {response.status_code})\n"
+    except Exception as e:
+        message = f"{timestamp} CRM is alive (GraphQL ERROR: {e})\n"
+
+    with open(log_path, "a") as log_file:
+        log_file.write(message)
